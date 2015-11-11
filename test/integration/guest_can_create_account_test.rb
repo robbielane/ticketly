e40a74728781a3c2f2f55ce19a7de1da4ccdd763
Nicole Hall <nicolealexandrahall@gmail.com>
2015-11-10 18:47:56 -0700

    add 3 tests for user authentication functionality: -account can be created -user can be created -registered user can login

--- test/integration/guest_can_create_account_test.rb
+++ test/integration/guest_can_create_account_test.rb
@@ -8,4 +8,36 @@ class GuestCanCreateAccountTest < ActionDispatch::IntegrationTest
     assert page.has_content?("Global Pursuits")
     assert page.has_button?("Pursue Your Passion")
   end
+
+  test "account can be created" do
+    visit root_path
+    assert page.has_content?("Global Pursuits")
+    assert page.has_button?("Create Account")
+    click_link "Create Account"
+    assert_equal new_user_path ,current_path
+
+  end
+
+  test "a user can be created" do
+    visit new_user_path
+    fill_in "Username", with: "Nicole"
+    fill_in "Password", with: "password"
+
+    click_button "Create Account"
+
+    assert page.has_content?("Welcome, Nicole")
+  end
+
+  test "a registered user can log in" do
+    user = User.create(username: "Nicole", password: "password")
+
+    visit login_path
+
+    fill_in "Username", with: user.username
+    fill_in "Password", with: "password"
+
+    click_button "Login"
+
+    assert page.has_content?("Welcome, Nicole")
+  end
 end
