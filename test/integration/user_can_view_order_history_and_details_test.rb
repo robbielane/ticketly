6d3aef17652f71b41961b0d864f22c8d6956f2c1
acareaga <aaroncareaga@gmail.com>
2015-11-12 08:08:46 -0700

    Added a few notes

--- test/integration/user_can_view_order_history_and_details_test.rb
+++ test/integration/user_can_view_order_history_and_details_test.rb
@@ -17,5 +17,9 @@ class UserCanViewOrderHistoryAndDetailsTest < ActionDispatch::IntegrationTest
     assert page.has_content?("Status: Completed")
     assert page.has_content? ("Ordered: 11/11/15 at 8:47 PM")
     assert page.has_content? ("Fulfilled: 11/12/15 at 7:20 AM")
+
+    # And if any of the items in the order were retired from the menu
+    # Then they should still be able to access the item page
+    # But they should not be able to add the item to their cart
   end
 end
