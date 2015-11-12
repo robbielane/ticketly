d4ee9eec4b42414dbde66848d6b7d6a06d86a34b
acareaga <aaroncareaga@gmail.com>
2015-11-12 14:56:53 -0700

    Fixed login form, revamped navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
       link_to "Login", login_path
     end
   end
+
+  def join_dashboard_path
+    if current_user
+      link_to "Logged in as #{current_user.name}", dashboard_path
+    else
+      link_to "Join", new_user_path
+    end
+  end
 end
