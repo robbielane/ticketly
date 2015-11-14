4bc10916ad94c0f59cec4f50cf4fef1396735301
acareaga <aaroncareaga@gmail.com>
2015-11-14 10:13:37 -0700

    Added authenticated user test for logging in as a regular user

--- test/integration/authenticated_users_security_test.rb
+++ test/integration/authenticated_users_security_test.rb
@@ -15,16 +15,29 @@ class AuthenticatedUsersSecurityTest < ActionDispatch::IntegrationTest
     assert_equal "/login", current_path
   end
 
-  test "authenticated user cannot access admin dashboard and create/update/delete trips" do
-    user = User.create(name: "Nicole", username: "cole", password: "password", role: 0)
-    admin = User.create(name: "Aaron", username: "AaronC", password: "password", role: 1)
-    visit admin_login_path
-    refute
-  end
-
   test "authenticated user cannot make themself an admin" do
-    skip
+    visit root_path
+    click_button "Apply for Membership"
+
+    fill_in "Username", with: "cole"
+    fill_in "Name", with: "Cole"
+    fill_in "Password", with: "pass"
+
+    refute page.has_content?("Role")
+    click_button "Create Account"
   end
 
-  
+  test "authenticated user cannot access admin dashboard and create/update/delete trips" do
+    user = User.create(name: "Nicole", username: "cole", password: "password")
+
+    visit root_path
+    fill_in "Username", with: "cole"
+    fill_in "Password", with: "pass"
+    click_button "Login"
+
+    refute page.has_content?("Admin Dashboard")
+    refute page.has_content?("Create Pursuits")
+    refute page.has_content?("Edit Pursuits")
+    refute page.has_content?("Delete Pursuits")
+  end
 end
