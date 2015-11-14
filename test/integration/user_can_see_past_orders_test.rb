f4f8915f04a37d1e540b04749e1ac21863c31e63
Torie Joy-Warren <victoria.joywarren@gmail.com>
2015-11-14 11:54:28 -0700

    Add start of test for viewing a past order

--- test/integration/user_can_see_past_orders_test.rb
+++ test/integration/user_can_see_past_orders_test.rb
@@ -57,4 +57,26 @@ class UserCanSeePastOrdersTest < ActionDispatch::IntegrationTest
       assert page.has_content?("#{Order.first.created_at.strftime("%B %d, %Y")}")
     end
   end
+
+  test "authenticated user can see individual past orders" do
+    # Background: An existing user that has one previous order
+    checkout_user(1)
+    #   As an authenticated user
+    #   When I visit "/orders"
+    visit_path orders_path
+
+    click_link "View order details"
+    assert_equal  , current_path
+
+    #   Then I should see each item that was order with the quantity and line-item subtotals
+    #   And I should see links to each item's show page
+    #   And I should see the current status of the order (ordered, paid, cancelled, completed)
+    #   And I should see the total price for the order
+    #   And I should see the date/time that the order was submitted
+    #   If the order was completed or cancelled
+    #   Then I should see a timestamp when the action took place
+    #   And if any of the items in the order were retired from the menu
+    #   Then they should still be able to access the item page
+    #   But they should not be able to add the item to their cart
+  end
 end
