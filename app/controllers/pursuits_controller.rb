1d991c0ca961d543e3553b09cd3941ec77c41aeb
acareaga <aaroncareaga@gmail.com>
2015-11-17 08:40:43 -0700

    Need some help :)

--- app/controllers/pursuits_controller.rb
+++ app/controllers/pursuits_controller.rb
@@ -1,5 +1,5 @@
 class PursuitsController < ApplicationController
-  before_action :require_admin, only: [:new, :create]
+  # before_action :require_admin, only: [:new, :create]
 
   def index
     @activities = Activity.all
@@ -9,46 +9,46 @@ class PursuitsController < ApplicationController
     @pursuit = Pursuit.find(params[:id])
   end
 
-  def new
-    @pursuit = Pursuit.new
-  end
+  # def new
+  #   @pursuit = Pursuit.new
+  # end
+  #
+  # def create
+  #   activity = Activity.find_or_create_by(name: params[:pursuit][:activity])
+  #   @pursuit = activity.pursuits.new(pursuit_params)
+  #   if @pursuit.save
+  #     flash[:notice] = "The pursuit '#{@pursuit.name}' has been created"
+  #     redirect_to @pursuit
+  #   else
+  #     flash[:notice] = @pursuit.errors.full_messages.join(", ")
+  #     redirect_to new_pursuit_path
+  #   end
+  # end
+  #
+  # def edit
+  #   @pursuit = Pursuit.find(params[:id])
+  # end
+  #
+  # def update
+  #   @pursuit = Pursuit.find(params[:id])
+  #   if @pursuit.update(pursuit_params)
+  #     flash.notice = "Pursuit Updated!"
+  #     redirect_to admin_pursuits_path
+  #   else
+  #     flash.now[:errors] = @pursuit.errors.full_messages.join(" ,")
+  #     render :edit
+  #   end
+  # end
+  #
+  # def destroy
+  #   @pursuit = Pursuit.find(params[:id])
+  #   @pursuit.destroy
+  #   redirect_to admin_pursuits_path
+  # end
 
-  def create
-    activity = Activity.find_or_create_by(name: params[:pursuit][:activity])
-    @pursuit = activity.pursuits.new(pursuit_params)
-    if @pursuit.save
-      flash[:notice] = "The pursuit '#{@pursuit.name}' has been created"
-      redirect_to @pursuit
-    else
-      flash[:notice] = @pursuit.errors.full_messages.join(", ")
-      redirect_to new_pursuit_path
-    end
-  end
-
-  def edit
-    @pursuit = Pursuit.find(params[:id])
-  end
-
-  def update
-    @pursuit = Pursuit.find(params[:id])
-    if @pursuit.update(pursuit_params)
-      flash.notice = "Pursuit Updated!"
-      redirect_to admin_pursuits_path
-    else
-      flash.now[:errors] = @pursuit.errors.full_messages.join(" ,")
-      render :edit
-    end
-  end
-
-  def destroy
-    @pursuit = Pursuit.find(params[:id])
-    @pursuit.destroy
-    redirect_to admin_pursuits_path
-  end
-
-  private
-
-  def pursuit_params
-    params.require(:pursuit).permit(:name, :description, :price)
-  end
+  # private
+  #
+  # def pursuit_params
+  #   params.require(:pursuit).permit(:name, :description, :price)
+  # end
 end
