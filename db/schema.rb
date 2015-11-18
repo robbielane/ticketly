d2a7d4545da56aba0b3c83bd69e3d1ce6fdcce7b
Torie Joy-Warren <victoria.joywarren@gmail.com>
2015-11-18 13:53:29 -0700

    Remove interests column from users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151118170920) do
+ActiveRecord::Schema.define(version: 20151118205250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,7 +65,6 @@ ActiveRecord::Schema.define(version: 20151118170920) do
     t.string   "username"
     t.string   "name"
     t.string   "password_digest"
-    t.string   "interests"
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
     t.integer  "role",            default: 0
