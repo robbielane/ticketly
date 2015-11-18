36271eb427975be175e9075041a876dcbfcc5f31
Torie Joy-Warren <victoria.joywarren@gmail.com>
2015-11-18 15:02:39 -0700

    Refactor pursuits helpers

--- app/helpers/pursuits_helper.rb
+++ app/helpers/pursuits_helper.rb
@@ -3,13 +3,19 @@ module PursuitsHelper
     if current_user
       if current_user.admin?
         link_to "Edit Pursuit", edit_admin_pursuit_path(@pursuit.id)
-      elsif !current_user.admin?
-        if @pursuit.retired?
-          content_tag :p, "This pursuit has been retired and may no longer be purchased."
-        else
-          link_to "Purchase Trip", new_cart_pursuit_path(id: @pursuit.id)
-        end
+      else
+        return_purchase_link_or_retired_notice
       end
+    else
+      return_purchase_link_or_retired_notice
+    end
+  end
+
+  def return_purchase_link_or_retired_notice
+    if @pursuit.retired?
+      content_tag :p, "This pursuit has been retired and may no longer be purchased."
+    else
+      link_to "Purchase Trip", new_cart_pursuit_path(id: @pursuit.id)
     end
   end
 end
