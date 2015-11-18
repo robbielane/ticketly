5d89bb54f46af5819f89718cfe72f0132b9b09e4
acareaga <aaroncareaga@gmail.com>
2015-11-17 20:42:27 -0700

    Admin order table updated, links in place, need to add filtering for jQuery on status

--- test/integration/admin_orders_test.rb
+++ test/integration/admin_orders_test.rb
@@ -9,20 +9,32 @@ class AdminOrdersTest < ActionDispatch::IntegrationTest
     login_admin
 
     assert admin_dashboard_path, current_path
-    assert page.has_content?("Hiking")
+
+    assert page.has_content?("Order ID")
     assert page.has_content?("Pending")
 
-    click_link("Pending")
+    click_link("Ordered")
+save_and_open_page
+    # I can click on "cancel" on individual orders which are "paid" or "ordered"
+    assert page.has_content?("Ordered")
 
-    assert_equal "/admin/orders/#{Order.first.id}", current_path
-    assert page.has_content?("Details")
+    within("#update_status") do
+      click_button "cancel"
+    end
 
-  #   And I can filter orders to display by each status type  ("Ordered", "Paid", "Cancelled", "Completed")
-  #   And I can see the total number of orders for each status ("Ordered", "Paid", "Cancelled", "Completed")
+    # I can click on "mark as paid" on orders that are "ordered"
+    assert page.has_content?("Ordered")
 
-  # - I can click on "cancel" on individual orders which are "paid" or "ordered"
-  # - I can click on "mark as paid" on orders that are "ordered"
-  # - I can click on "mark as completed" on orders that are "paid"
+    within("#update_status") do
+      click_button "mark as paid"
+    end
+
+    # I can click on "mark as completed" on orders that are "paid"
+    assert page.has_content?("Paid")
+
+    within("#update_status") do
+      click_button "mark as completed"
+    end
   end
 
   test "admin can view an individual order" do
