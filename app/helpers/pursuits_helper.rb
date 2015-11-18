1c12f453f5f39e242db65ac663ef10046c185b54
Torie Joy-Warren <victoria.joywarren@gmail.com>
2015-11-18 14:20:51 -0700

    Add edit button to pursuit view for admin and remove purchase button

--- app/helpers/pursuits_helper.rb
+++ app/helpers/pursuits_helper.rb
@@ -2,8 +2,10 @@ module PursuitsHelper
   def purchase_link_or_retired_notice
     if @pursuit.retired?
       content_tag :p, "This pursuit has been retired and may no longer be purchased."
-    else
+    elsif !current_user.admin?
       link_to "Purchase Trip", new_cart_pursuit_path(id: @pursuit.id)
+    elsif current_user.admin?
+      link_to "Edit Pursuit", edit_admin_pursuit_path(@pursuit.id)
     end
   end
 end
