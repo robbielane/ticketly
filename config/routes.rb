678c818dba0dab7d8ecac10e5db9f8254b1af148
joshuajhun <joshuajhun@gmail.com>
2015-11-30 18:56:00 -0700

    changed more pursuits into tickets

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "home#index"
-  resources :pursuits, only: [:index, :show]
+  resources :tickets, only: [:index, :show]
   resources :cart_pursuits, only: [:new, :create]
   resources :users, only: [:new, :create, :show, :edit, :update, :destroy]
   resources :orders, only: [:index, :show]
