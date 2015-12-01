fd6ce314a6126ac3b36de3375c3bf9e7cf6bb29c
adamki <adajensen@gmail.com>
2015-12-01 13:57:51 -0700

    added Faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+class Seed
+  def initialize
+    generate_users
+    generate_orders
+    #generate_pursuits
+  end
 
-Pursuit.create(name: "Jetskiing", description: "Fun in the sun!", price: 150)
-Pursuit.create(name: "Skiing", description: "Powder snow in the Alps", price: 750)
-Pursuit.create(name: "Hiking", description: "South America tour through Argentina & Chile", price: 1150)
+  def generate_users
+    50.times do |i|
+      user = User.create!(
+        name: Faker::Name.name,
+        username: "#{Faker::Internet.user_name}#{i}", 
+        password: "password"
+      )
+      puts "User #{i}:#{user.username} completed!"
+    end
+  end
 
-User.create(username: "aaron", name: "Aaron", password: "pass")
-User.create(username: "torie", name: "Torie", password: "pass")
-User.create(username: "cole", name: "Cole", password: "pass")
 
-Order.create(user_id: 1, status: "Completed", total: 1000, created_at: "2015-10-18 21:56:18", updated_at: "2015-10-18 21:56:18")
-Order.create(user_id: 2, status: "Paid", total: 10000, created_at: "2015-09-18 21:56:18", updated_at: "2015-09-18 21:56:18")
-Order.create(user_id: 3, status: "Cancelled", total: 8000, created_at: "2015-11-18 21:56:18", updated_at: "2015-11-18 21:56:18")
-Order.create(user_id: 4, status: "Pending", total: 3000, created_at: "2015-08-18 21:56:18", updated_at: "2015-08-18 21:56:18")
+  def generate_pursuits
+    Pursuit.create(name: "Jetskiing", description: "Fun in the sun!", price: 150)
+    Pursuit.create(name: "Skiing", description: "Powder snow in the Alps", price: 750)
+    Pursuit.create(name: "Hiking", description: "South America tour through Argentina & Chile", price: 1150)
+  end
+
+  def generate_orders
+    user_count = User.count
+    status_collection = %w(Completed Paid Cancelled Pending)
+    100.times do |i|
+      user = User.offset(Random.new.rand(1..user_count)).limit(1)
+      5.times do |i|
+        user.orders << Order.create!(
+                                      user_id: user,
+                                      status: status_collection.sample,
+                                      total: Faker::Commerce.price,
+                                    )
+        puts "Order #{i}: Order for #{user.name} created!"
+      end
+    end
+  end
+end
+
+Seed.new
