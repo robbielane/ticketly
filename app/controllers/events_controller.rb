60d9a531e512f52338515b77fcb97450e908250f
Robbie Lane <lane.robbie@gmail.com>
2015-12-02 21:21:35 -0700

    style search bar

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,6 @@ class EventsController < ApplicationController
   def index
     if params[:query].present?
       @events = Event.search(params[:query], name: params[:name])
-                     .paginate(page: params[:page], per_page: 20)
     else
       @events = Event.all.paginate(page: params[:page], per_page: 20)
     end
