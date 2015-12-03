f2545d79b1a3512071534065513227f590f1d5ed
adamki <adajensen@gmail.com>
2015-12-03 13:58:25 -0700

    commit before deleting views/cart_pursuites dir

--- test/integration/registered_user_can_purchase_tickets_test.rb
+++ test/integration/registered_user_can_purchase_tickets_test.rb
@@ -17,4 +17,21 @@ class RegisteredUserCanPurchaseTicketsTest < ActionDispatch::IntegrationTest
     assert page.has_content?("Order was successfully placed")
   end
 
+
+  test "registered user can remove ticket to cart" do
+    ticket = tickets(:one)
+    create_and_login_user
+    visit root_path
+    click_link "Events"
+    click_link "Disney Frozen On Ice"
+    within("##{ticket.id}") do
+      click_on "Purchase"
+    end
+    click_on "Cart"
+    assert cart_path, current_path 
+    click_on "Remove"
+    result = "You have removed the trip Disney Frozen On Ice from your cart"
+    assert page.has_content?("#{result}")
+  end
 end
+
