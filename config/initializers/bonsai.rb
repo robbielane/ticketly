ff3baa78a430de180dea01f0ef8d0f0e07f3d21a
Robbie Lane <lane.robbie@gmail.com>
2015-12-03 00:17:13 -0700

    Add Bonsai initializer

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,3 +1,3 @@
-if ENV[‘BONSAI_URL’]
-     Searchkick.client = Elasticsearch::Client.new({url: ENV['BONSAI_URL’], logs: true})
+if ENV['BONSAI_URL']
+  Searchkick.client = Elasticsearch::Client.new({url: ENV['BONSAI_URL'], logs: true})
 end
