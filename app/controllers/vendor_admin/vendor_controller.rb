94068bc3f2ef709c01657ad89b4757e2f674d88f
joshuajhun <joshuajhun@gmail.com>
2015-12-03 17:06:59 -0700

    implemented routes for the vendor admin. Vendor admin can currently it's account information. we also changed the routes to vendor admin paths, we scoped the routes, we are currently working on getting the rest of the test to pass

--- app/controllers/vendor_admin/vendor_controller.rb
+++ app/controllers/vendor_admin/vendor_controller.rb
@@ -1,5 +1,5 @@
-class VendorAdminController < ApplicationController
-  before_action :require_vendor_admin
+class VendorAdmin::VendorController < ApplicationController
+  before_action :current_vendor?
 
   def dashboard
     @vendor_admin = current_user
