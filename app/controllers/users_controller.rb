021c5a8d2fee0349e3ca68c5a3f5ddbebfb5ff02
Robbie Lane <lane.robbie@gmail.com>
2015-12-04 14:05:13 -0700

    Fix sign up role assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      @users.roles << Role.find(1)
+      @user.roles << Role.find(1)
       session[:user_id] = @user.id
       redirect_to "/dashboard"
     else
