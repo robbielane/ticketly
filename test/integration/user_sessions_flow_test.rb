b030f265852d2f3b37ce15ae1926e76b01d0fe8f
adamki <adajensen@gmail.com>
2015-12-06 20:37:40 -0700

    user login and logout functionality tested.

---
+++ test/integration/user_sessions_flow_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class UserSessionsFlowTest < ActionDispatch::IntegrationTest
+
+  test "user redirected to login path when givin invalid credentials" do
+    user = create_user
+    visit root_path
+    incorrect_password = "ilovedixtix"
+
+    click_link "Login"
+    fill_in "Username", with: "cole"
+    fill_in "Password", with: incorrect_password
+    click_button "Login"
+
+    assert page.has_content?("Invalid login credentials")
+    assert login_path, current_path
+  end
+    
+  test "logged in user can log out" do
+    create_and_login_user
+
+    click_on "Logout"
+    assert root_path, current_path
+  end
+end
