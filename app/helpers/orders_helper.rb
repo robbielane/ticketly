2a37c976c956dc397cf5c37cd257d33200ab2f0d
acareaga <aaroncareaga@gmail.com>
2015-12-08 15:37:09 -0700

    Fixed ticket show page

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -5,20 +5,20 @@ module OrdersHelper
   end
 
   def tickets_names_and_travellers(order)
-    order.tickets.map { |ticket| format_name_and_travellers(order, ticket) }
+    order.tickets.map { |ticket| format_name_and_tickets(order, ticket) }
   end
 
-  def ticket_name_and_travellers(order, target_ticket)
+  def ticket_name_and_quantity(order, target_ticket)
     ticket_name = order.tickets.map do |ticket|
       next unless target_ticket.id.to_i == ticket.id
-      format_name_and_travellers(order, ticket)
+      format_name_and_tickets(order, ticket)
     end
     ticket_name.compact[0]
   end
 
-  def format_name_and_travellers(order, ticket)
-    num_travellers = OrderTicket.find_by(ticket_id: ticket.id, order_id: order.id).quantity
-    "#{ticket.name} (Tickets: #{num_travellers || 1})"
+  def format_name_and_tickets(order, ticket)
+    OrderTicket.find_by(ticket_id: ticket.id, order_id: order.id)
+    "#{Event.find(ticket.event_id).name}"
   end
 
   def filter_by_status
