416bfe1c9bb7dc87812612ff0e67d3924d632e36
acareaga <aaroncareaga@gmail.com>
2015-12-07 18:18:47 -0700

    Default vendor status to inactive

--- db/migrate/20151203024341_create_vendors.rb
+++ db/migrate/20151203024341_create_vendors.rb
@@ -1,7 +1,7 @@
   class CreateVendors < ActiveRecord::Migration
   def change
     create_table :vendors do |t|
-      t.integer :status
+      t.integer :status, default: 0
       t.string :name
       t.references :user, index: true, foreign_key: true
 
