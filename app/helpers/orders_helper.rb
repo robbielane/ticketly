6ce199866c8a5bea21497c95d711982b3be62faa
adamki <adajensen@gmail.com>
2015-12-09 15:33:26 -0700

    WIP

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,13 +1,4 @@
 module OrdersHelper
-  def order_tickets_list(order)
-    tickets = tickets_names_and_travellers(order)
-    tickets.join(",  <br>")
-  end
-
-  def tickets_names_and_travellers(order)
-    order.tickets.map { |ticket| format_name_and_tickets(order, ticket) }
-  end
-
   def ticket_name_and_quantity(order, target_ticket)
     ticket_name = order.tickets.map do |ticket|
       next unless target_ticket.id.to_i == ticket.id
@@ -19,9 +10,5 @@ module OrdersHelper
   def format_name_and_tickets(order, ticket)
     OrderTicket.find_by(ticket_id: ticket.id, order_id: order.id)
     "#{Event.find(ticket.event_id).name}"
-  end
-
-  def filter_by_status
-    Order.all.each { |order| order.status == "#{status}" }
   end
 end
